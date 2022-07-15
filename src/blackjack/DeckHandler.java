9bc41a9fe8c94ba068ad57e728b5e831ea25e121
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-15 15:19:39 -0600

    add dealTo method to deal card from deck

--- src/blackjack/DeckHandler.java
+++ src/blackjack/DeckHandler.java
@@ -1,8 +1,13 @@
 package blackjack;
 
+import java.util.Random;
+
 public class DeckHandler {
 
-	private final Card[] deck = new Card[52];
+	private final Random rng = new Random();
+	
+	private final int DECK_SIZE = 52;
+	private final Card[] deck = new Card[DECK_SIZE];
 
 	public DeckHandler() {
 		int i = 0;
@@ -11,6 +16,22 @@ public class DeckHandler {
 				deck[i] = new Card(v, s);
 				++i;
 			}
+	}
+	
+	public void dealTo(Player player) {
+		Card card;
+		
+		boolean alreadyUsed;
+		// makes sure the card hasn't already been dealt before dealing
+		do {
+			alreadyUsed = false;
+			card = deck[rng.nextInt(DECK_SIZE)];
+			for (Card c : deck)
+				if (c.isInUse())
+					alreadyUsed = true;
+		} while (alreadyUsed);
+		
+		player.deal(card);
 	}
 
 }
