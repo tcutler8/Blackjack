08a4c0a259874740575f870ee8ca3935792fa3ba
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-15 09:51:41 -0600

    constructor initialize 52-card deck

--- src/blackjack/DeckHandler.java
+++ src/blackjack/DeckHandler.java
@@ -2,8 +2,15 @@ package blackjack;
 
 public class DeckHandler {
  
-    public DeckHandler() {
-      
-    }
+	private Card[] deck = new Card[52];
+	
+	public DeckHandler() {
+		int i = 0;
+		for (cardSuit s : CardSuit.values())
+            for (CardValue v : CardValue.values()) {
+                deck[i] = new Card(v, s);
+                ++i;
+            }
+	}
   
 }
