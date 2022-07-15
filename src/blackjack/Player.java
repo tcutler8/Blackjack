645d53ab244687937c6e763599faf7dde2a227f7
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-15 15:49:26 -0600

    change to abstract & add getScore()

--- src/blackjack/Player.java
+++ src/blackjack/Player.java
@@ -3,29 +3,42 @@ package blackjack;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player {
+public abstract class Player {
 
-	private int balance;
 	private List<Card> hand = new ArrayList<>();
 	
-	public Player() {
-		
-	}
+	public void deal(Card card) {hand.add(card);}
 	
-	public void deal(Card card) {
-		
-	}
-	
-	public int getBalance() {
-		return balance;
-	}
-	
-	public List<Card> getHand() {
-		return hand;
-	}
+	public List<Card> getHand() {return hand;}
 	
 	public int getScore() {
-		return -1;
+		int acesCount = 0;
+		int score     = 0;
+
+		for (Card c : hand) 
+			switch (c.getValue()) {
+				case ACE:
+					++acesCount;
+					break;
+				case KING:
+				case QUEEN:
+				case JACK:
+				case TEN:   ++score;
+				case NINE:  ++score;
+				case EIGHT: ++score;
+				case SEVEN: ++score;
+				case SIX:   ++score;
+				case FIVE:  ++score;
+				case FOUR:  ++score;
+				case THREE: ++score;
+				case TWO:   ++score;
+				default:    ++score;
+			}
+		
+		if (score < (12 - acesCount) && acesCount > 0) 
+			return score += 11 + (acesCount -1);
+		else
+			return score += acesCount;
 	}
 	
 }
