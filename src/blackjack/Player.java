7ac9261a916dd5c9666b23b4d3577d1d6b6934a2
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-15 15:19:05 -0600

    basic outline of player

--- src/blackjack/Player.java
+++ src/blackjack/Player.java
@@ -1,13 +1,31 @@
 package blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
+	private int balance;
+	private List<Card> hand = new ArrayList<>();
+	
 	public Player() {
 		
 	}
 	
 	public void deal(Card card) {
 		
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public List<Card> getHand() {
+		return hand;
+	}
+	
+	public int getScore() {
+		return -1;
 	}
 	
 }
