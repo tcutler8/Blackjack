e4f5ed67b2825d811b9462585fba80973b35e722
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-15 09:56:11 -0600

    fix enum name beig incorrect

--- src/blackjack/DeckHandler.java
+++ src/blackjack/DeckHandler.java
@@ -1,16 +1,16 @@
 package blackjack;
 
 public class DeckHandler {
- 
-	private Card[] deck = new Card[52];
-	
+
+	private final Card[] deck = new Card[52];
+
 	public DeckHandler() {
 		int i = 0;
-		for (cardSuit s : CardSuit.values())
-            for (CardValue v : CardValue.values()) {
-                deck[i] = new Card(v, s);
-                ++i;
-            }
+		for (final CardSuit s : CardSuit.values())
+			for (final CardValue v : CardValue.values()) {
+				deck[i] = new Card(v, s);
+				++i;
+			}
 	}
-  
+
 }
