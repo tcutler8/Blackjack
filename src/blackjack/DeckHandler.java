514a12e3199085e373a5ec88b275cb622ffcc1e3
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-16 17:40:54 -0600

    add doc comments

--- src/blackjack/DeckHandler.java
+++ src/blackjack/DeckHandler.java
@@ -2,13 +2,24 @@ package blackjack;
 
 import java.util.Random;
 
+/**
+ * Handles a 52-card deck of playing cards.
+ * 
+ * @author tayson
+ */
 public class DeckHandler {
 
 	private final Random rng = new Random();
 	
+	/**
+	 * The number of cards in a deck of playing cards.
+	 */
 	public static final int DECK_SIZE = 52;
 	private final Card[] fullDeck = new Card[DECK_SIZE];
 
+	/**
+	 * Initialize the deck with all 52 cards.
+	 */
 	public DeckHandler() {
 		int i = 0;
 		for (final CardSuit s : CardSuit.values())
@@ -18,6 +29,9 @@ public class DeckHandler {
 			}
 	}
 	
+	/**
+	 * @return number of cards left in the deck.
+	 */
 	public int getCardsLeftCount() {
 		int count = DECK_SIZE;
 		for (Card c : fullDeck)
@@ -26,6 +40,11 @@ public class DeckHandler {
 		return count;
 	}
 	
+	/**
+	 * Deal a card from the deck to <code>player</code>.
+	 * 
+	 * @param player to deal to
+	 */
 	public void dealTo(Player player) {
 		int randInt;
 		do {
@@ -36,6 +55,9 @@ public class DeckHandler {
 		player.deal(fullDeck[randInt]);
 	}
 	
+	/**
+	 * Shuffle the deck. Allows all cards to be used again.
+	 */
 	public void shuffle() {
 		for (Card c : fullDeck)
 			c.use(false);
