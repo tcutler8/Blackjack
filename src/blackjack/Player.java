04bf20fb2fd7d7d86e236b9fc0598e60a8df4e46
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-16 18:00:20 -0600

    shorten getTotalScore() & add doc comments

--- src/blackjack/Player.java
+++ src/blackjack/Player.java
@@ -3,44 +3,58 @@ package blackjack;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Abstract class representing a player in Blackjack.
+ * 
+ * @author tayson
+ */
 public abstract class Player {
 
 	private List<Card> hand = new ArrayList<>();
 	
+	/**
+	 * Deal the Card to the Player.
+	 * @param card to deal
+	 */
 	public void deal(Card card) {hand.add(card);}
 	
+	/**
+	 * Remove all cards from the player's hands. 
+	 */
 	public void clearHand() {hand.clear();}
 	
+	/**
+	 * @return List of all cards in the Player's hand
+	 */
 	public List<Card> getHand() {return hand;}
 	
-	public int getScore() {
+	/**
+	 * @return total score of the Player's hand
+	 */
+	public int getTotalScore() {
 		int acesCount = 0;
 		int score     = 0;
 
 		for (Card c : hand) 
 			switch (c.getValue()) {
-				case ACE:
-					++acesCount;
-					break;
 				case KING:
 				case QUEEN:
 				case JACK:
-				case TEN:   ++score;
-				case NINE:  ++score;
-				case EIGHT: ++score;
-				case SEVEN: ++score;
-				case SIX:   ++score;
-				case FIVE:  ++score;
-				case FOUR:  ++score;
-				case THREE: ++score;
-				case TWO:   ++score;
-				default:    ++score;
+				case TEN:   score++;
+				case NINE:  score++;
+				case EIGHT: score++;
+				case SEVEN: score++;
+				case SIX:   score++;
+				case FIVE:  score++;	
+				case FOUR:  score++;
+				case THREE: score++;
+				case TWO:   score++;
+				case ACE:	score++; acesCount++;
 			}
 		
-		if (score < (12 - acesCount) && acesCount > 0) 
-			return score += 11 + (acesCount -1);
-		else
-			return score += acesCount;
+		// return the score, but add 10 to it if the score
+		// is less than 12 and there is at least 1 ace
+		return score += (((score < 12) && (acesCount > 0)) ? 10 : 0);
 	}
 	
 }
