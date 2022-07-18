4eac54c01ef56eb06096bbd7ad8519a6e45fd62d
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-18 13:35:15 -0600

    minor changes to style

--- src/blackjack/BlackjackGame.java
+++ src/blackjack/BlackjackGame.java
@@ -14,21 +14,23 @@ public class BlackjackGame {
 		
 		// Player1 starting chip count
 		System.out.println("Player starting chip count: " + player1.getBalance());
-		System.out.println("Enter bet amount: ");
+		System.out.print("Enter bet amount: ");
 		bet = scan.nextInt();
 		System.out.println();
 		
 		// Dealer starting out with two cards
 		deck.dealTo(dealer);
 		deck.dealTo(dealer);
-		System.out.println("Dealer showing: " + dealer.getFirstCard());
+		System.out.println("Dealer:");
+		System.out.println("Card showing:   " + dealer.getFirstCard());
+		System.out.println("Score of card:  " + dealer.getFirstCardScore());
 		System.out.println();
 		
 		// Player starting out with two cards
-		System.out.println("Player");
+		System.out.println("Player:");
 		deck.dealTo(player1);
 		deck.dealTo(player1);
-		System.out.println("Hand:	" + player1.getHand());
+		System.out.println("Hand:	" + player1.getHand().toString().replace("[", "").replace("]", ""));
 		System.out.println("Score:	" + player1.getTotalScore());
 		if (player1.getTotalScore() == 21) {
 			System.out.println("BLACKJACK!!!");
@@ -39,11 +41,12 @@ public class BlackjackGame {
 		// Player to hit or stay. This will be a button on the JPanel
 		// Type in "h" to take a hit and "s" to stay
 		while (hitOrStay.equals("h")) {
-			System.out.println("Hit (h) or Stay (s)?");
+			System.out.print("Hit (h) or Stay (s)?: ");
 			hitOrStay = scan.next();
+			System.out.println();
 			if (hitOrStay.equals("h")) {
 				deck.dealTo(player1);
-				System.out.println("Hand:	" + player1.getHand());
+				System.out.println("Hand:	" + player1.getHand().toString().replace("[", "").replace("]", ""));
 			}
 			if (player1.getTotalScore() == 21) {
 				hitOrStay = "s";
@@ -69,8 +72,8 @@ public class BlackjackGame {
 		}
 		
 		// Show dealer's hand
-		System.out.println("Dealer Hand:	" + dealer.getHand());
-		System.out.println("Score:	" + dealer.getTotalScore());
+		System.out.println("Dealer Hand:  " + dealer.getHand().toString().replace("[", "").replace("]", ""));
+		System.out.println("Score:	      " + dealer.getTotalScore());
 		System.out.println();
 
 		// Test to see if player wins
