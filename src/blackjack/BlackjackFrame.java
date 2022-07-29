21b9fcb73d9707bc09a6b2019084462344dff95f
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-29 15:27:31 -0600

    clean up unnecessary code

--- src/blackjack/BlackjackFrame.java
+++ src/blackjack/BlackjackFrame.java
@@ -21,29 +21,20 @@ public class BlackjackFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 720);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{1000, 0};
-		gbl_contentPane.rowHeights = new int[]{620, 100, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{1000};
+		gbl_contentPane.rowHeights = new int[]{620, 100};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JPanel gamePanel = new GamePanel();
 		GridBagConstraints gbc_gamePanel = new GridBagConstraints();
 		gbc_gamePanel.fill = GridBagConstraints.BOTH;
-		gbc_gamePanel.insets = new Insets(0, 0, 0, 0);
-		gbc_gamePanel.gridx = 0;
-		gbc_gamePanel.gridy = 0;
 		contentPane.add(gamePanel, gbc_gamePanel);
 		
 		JPanel actionPanel = new ActionPanel();
-		actionPanel.setOpaque(true);
 		GridBagConstraints gbc_actionPanel = new GridBagConstraints();
-		gbc_actionPanel.anchor = GridBagConstraints.NORTH;
 		gbc_actionPanel.fill = GridBagConstraints.BOTH;
-		gbc_actionPanel.gridx = 0;
 		gbc_actionPanel.gridy = 1;
 		contentPane.add(actionPanel, gbc_actionPanel);
 	}
