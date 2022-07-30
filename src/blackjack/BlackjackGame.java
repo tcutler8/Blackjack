12ab94fef76285250c08142b88c228ad7a2f4008
tcutler8 <tcutler8@bruinmail.slcc.edu>
2022-07-30 16:51:03 -0600

    show cards when dealt

--- src/blackjack/BlackjackGame.java
+++ src/blackjack/BlackjackGame.java
@@ -1,45 +1,106 @@
 package blackjack;
 
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * Game of BlackJack between the dealer (computer) and the user. 
  * 
  * @author mark + tayson
  */
-public class BlackjackGame {
+public class BlackjackGame extends  JFrame {
 	
-	public final static User player = new User(500);
+	private static final long serialVersionUID = -7888782584683513688L;
+	
+	public final static User user = new User(500);
 	private static Dealer dealer = new Dealer();
 	private static DeckHandler deck = new DeckHandler();
 	private static int bet = 0;
 
+	private static BlackjackGame frame;
+	
+	private static GamePanel gamePanel;
+	private static ActionPanel actionPanel;
+	
+	//
+	// SET UP FRAME
+	//
+	
 	public static void main(String[] args) {
-
 		System.setProperty("awt.useSystemAAFontSettings","on"); // set anti-aliasing
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					BlackjackFrame frame = new BlackjackFrame();
+					frame = new BlackjackGame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		
 	}
 	
-	public static void setBet(int bet) {BlackjackGame.bet = bet;}
+	public BlackjackGame() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 780);
+		JPanel contentPane = new JPanel();
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{1000};
+		gbl_contentPane.rowHeights = new int[]{660, 120};
+		contentPane.setLayout(gbl_contentPane);
+		
+		gamePanel = new GamePanel();
+		GridBagConstraints gbc_gamePanel = new GridBagConstraints();
+		gbc_gamePanel.fill = GridBagConstraints.BOTH;
+		contentPane.add(gamePanel, gbc_gamePanel);
+		
+		actionPanel = new ActionPanel();
+		GridBagConstraints gbc_actionPanel = new GridBagConstraints();
+		gbc_actionPanel.fill = GridBagConstraints.BOTH;
+		gbc_actionPanel.gridy = 1;
+		contentPane.add(actionPanel, gbc_actionPanel);
+	}
+	
+	//
+	// GAME LOGIC
+	//
+	
+	public static void startGame(int bet) {
+		BlackjackGame.bet = bet;
+		dealCards();
+		gamePanel.placeCardsOnTable(dealer.getFirstCard(), user.getHand());
+	}
 
+	public static int getChips() {return user.getBalance();}
+	
+	private static void dealCards() {
+		final int CARDS_TO_DEAL = 2;
+		for (int i = 0; i < CARDS_TO_DEAL; ++i) {
+			deck.dealTo(user);
+			deck.dealTo(dealer);
+		}
+		System.out.println(user.getHand());
+		System.out.println(dealer.getHand());
+	}
+
+	//
+	// CONSOLE TEST CLIENT
+	//
+	
 	@SuppressWarnings("unused")
 	private static void consoleTestClient() {
 		Scanner     scan 	 = new Scanner(System.in);
 		String      hitOrStay = "h";
 		
 		// Player1 starting chip count
-		System.out.println("Player starting chip count: " + player.getBalance());
+		System.out.println("Player starting chip count: " + user.getBalance());
 		System.out.print("Enter bet amount: ");
 		bet = scan.nextInt();
 		System.out.println();
@@ -54,11 +115,11 @@ public class BlackjackGame {
 		
 		// Player starting out with two cards
 		System.out.println("Player:");
-		deck.dealTo(player);
-		deck.dealTo(player);
-		System.out.println("Hand:	" + player.getHand().toString().replace("[", "").replace("]", ""));
-		System.out.println("Score:	" + player.getTotalScore());
-		if (player.getTotalScore() == 21) {
+		deck.dealTo(user);
+		deck.dealTo(user);
+		System.out.println("Hand:	" + user.getHand().toString().replace("[", "").replace("]", ""));
+		System.out.println("Score:	" + user.getTotalScore());
+		if (user.getTotalScore() == 21) {
 			System.out.println("BLACKJACK!!!");
 			hitOrStay = "s";
 		}
@@ -71,22 +132,22 @@ public class BlackjackGame {
 			hitOrStay = scan.next();
 			System.out.println();
 			if (hitOrStay.equals("h")) {
-				deck.dealTo(player);
-				System.out.println("Hand:	" + player.getHand().toString().replace("[", "").replace("]", ""));
+				deck.dealTo(user);
+				System.out.println("Hand:	" + user.getHand().toString().replace("[", "").replace("]", ""));
 			}
-			if (player.getTotalScore() == 21) {
+			if (user.getTotalScore() == 21) {
 				hitOrStay = "s";
 				System.out.println("BLACKJACK!!!");
 			}
 			if (hitOrStay.equals("s")) {
-				System.out.println("Player will stay with a score of " + player.getTotalScore());
+				System.out.println("Player will stay with a score of " + user.getTotalScore());
 			}
-			if (player.getTotalScore() > 21) {
-				System.out.println("Score:	"	+ player.getTotalScore() + " BUST!");
+			if (user.getTotalScore() > 21) {
+				System.out.println("Score:	"	+ user.getTotalScore() + " BUST!");
 				hitOrStay = "s";
 			}
 			else {
-				System.out.println("Score:	" + player.getTotalScore());
+				System.out.println("Score:	" + user.getTotalScore());
 			}
 		}
 		System.out.println();
@@ -103,25 +164,25 @@ public class BlackjackGame {
 		System.out.println();
 
 		// Test to see if player wins
-		if (dealer.getTotalScore() == player.getTotalScore()) {
+		if (dealer.getTotalScore() == user.getTotalScore()) {
 			System.out.println("Push!!"); // Push
 		}
-		else if (dealer.getTotalScore() > 21 && player.getTotalScore() < 22) {
+		else if (dealer.getTotalScore() > 21 && user.getTotalScore() < 22) {
 			System.out.println("Player wins!!");
-			player.changeBalance(bet); // Player wins bet
+			user.changeBalance(bet); // Player wins bet
 		}
-		else if (player.getTotalScore() < 22 && player.getTotalScore() > dealer.getTotalScore()) {
+		else if (user.getTotalScore() < 22 && user.getTotalScore() > dealer.getTotalScore()) {
 			System.out.println("Player wins!!");
-			player.changeBalance(bet); // Player wins bet
+			user.changeBalance(bet); // Player wins bet
 		}
 		else {
 			System.out.println("Player loses!!");
-			player.changeBalance(bet * -1); // Player loses bet
+			user.changeBalance(bet * -1); // Player loses bet
 		}
 		System.out.println();
 		
 		// Show player's updated balance
-		System.out.println("Player current chip count: " + player.getBalance());
+		System.out.println("Player current chip count: " + user.getBalance());
 		
 		// This is to show how many cards left in the deck
 		System.out.print("Amount of cards left in the deck: ");
@@ -129,14 +190,10 @@ public class BlackjackGame {
 		
 		// Return cards and shuffle the deck 
 		dealer.clearHand();
-		player.clearHand();
+		user.clearHand();
 		deck.shuffle();
 		System.out.print("Amount of cards in new deck: ");
 		System.out.println(deck.getCardsLeftCount());
-	}
-
-	public static int getChips() {
-		return player.getBalance();
 	}
 
 }
--- src/blackjack/GamePanel.java
+++ src/blackjack/GamePanel.java
@@ -1,17 +1,69 @@
 package blackjack;
 
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class GamePanel extends JPanel {
 
 	private static final long serialVersionUID = -4346803565314594840L;
 
+	JPanel dealerHand;
+	JPanel userHand;
+	
+	JLabel[] dealerCards;
+	JLabel[] userCards;
+	
 	/**
 	 * Create the panel.
 	 */
 	public GamePanel() {
 		setBackground(new Color(0, 102, 0));
+		setLayout(new BorderLayout());
+		
+		dealerHand = new JPanel();
+		dealerHand.setLayout(new FlowLayout(FlowLayout.CENTER, -110, 10));
+		dealerHand.setOpaque(false);
+		add(dealerHand, BorderLayout.NORTH);
+			
+			dealerCards = new JLabel[11];
+			for (int i = 0; i < dealerCards.length; ++i) {
+				dealerCards[i] = new JLabel();
+				dealerCards[i].setVisible(false);
+				dealerHand.add(dealerCards[i]);
+			}
+		
+		userHand = new JPanel();
+		userHand.setLayout(new FlowLayout(FlowLayout.CENTER, -110, 0));
+		userHand.setOpaque(false);
+		add(userHand, BorderLayout.SOUTH);
+
+			userCards = new JLabel[11];
+			for (int i = 0; i < userCards.length; ++i) {
+				userCards[i] = new JLabel();
+				userCards[i].setVisible(false);
+				userHand.add(userCards[i]);
+			}
 	}
 
+	public void placeCardsOnTable(Card dealerCard, List<Card> userCard) {
+		dealerCards[0].setIcon(dealerCard.getFaceImage());
+		dealerCards[1].setIcon(Card.getBackImage());
+		
+		
+		for (int i = 0; i < userCard.size(); ++i) {
+			userCards[i].setIcon(userCard.get(i).getFaceImage());
+			
+			dealerCards[i].setVisible(true);
+			userCards[i].setVisible(true);
+		}
+		
+		
+	}
+	
 }
